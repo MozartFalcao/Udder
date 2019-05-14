53c5fd95b487f7fcabddb2c987d4ea3cb3b9c0b7
Mozart Falcão <mozartmc2@gmail.com>
2019-05-14 15:11:18 -0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Mozart Falcão on 14/05/19.
+//  Copyright © 2019 Mozart Falcão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
